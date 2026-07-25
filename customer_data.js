c266c20b91ec74e184d1f9488d72c9d12cb461e9
mohamed967saeed-cpu <mohamed967saeed@gmail.com>
2026-07-26 01:46:18 +0300

    first commit

---
+++ customer_data.js
@@ -0,0 +1,7 @@
+let cus_data=[{Name:"Mohamed Saeed",Email:"mohamed@gmail.com",Phone:"01067480967",DR:"DR.A",Treatment:"Tooth Extraction",number:1,status:"Waiting"},
+{Name:"Saeed Ahmed",Email:"saeed@gmail.com",Phone:"01020251105",DR:"DR.M",Treatment:"Root Canal Treatment",number:1,status:"Waiting"},
+{Name:"Moumud Ahmed",Email:"muhmod@gmail.com",Phone:"01091411277",DR:"DR.A",Treatment:"Tooth Extraction",number:2,status:"Completed"}]
+;
+
+
+export default cus_data;    
---
+++ registration-form.js
@@ -0,0 +1,49 @@
+import cus_data from "./customer_data.js"
+console.log("JavaScript Loaded");
+console.log(cus_data)
+
+const form=document.getElementById('Fo')
+
+const name=document.getElementById('Name')
+const email=document.getElementById('Email')
+
+const tel=document.getElementById('tel')
+const DrA=document.getElementById('DR.A')
+const DrM=document.getElementById('DR.M')
+const Treatment=document.getElementById('Sel')
+const mes=document.getElementById('mess')
+
+function isFormValid(){
+    return form.checkValidity();
+}
+
+function handleRegistrationSubmit(event){
+    event.preventDefault();
+    if(!isFormValid()){
+        mes.textContent="Please complete every field with valid data";
+        form.reportValidity();
+        return
+
+    }
+    else{console.log(name.value)
+        console.log(email.value)
+        console.log(tel.value)
+        console.log(DrA.value)
+        console.log(DrM.value)
+        console.log(Treatment.value)
+            
+    }
+    
+
+
+}
+
+form.addEventListener('submit', handleRegistrationSubmit)
+
+
+console.log(name.value)
+console.log(email.value)
+console.log(tel.value)
+console.log(DrA.value)
+console.log(DrM.value)
+console.log(Treatment.value)
