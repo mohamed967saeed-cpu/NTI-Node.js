c38383a8511b39ddc07c32a9963a90d6fc832b9e
mohamed967saeed-cpu <mohamed967saeed@gmail.com>
2026-07-29 00:59:58 +0300

    Merge branch 'main' of https://github.com/mohamed967saeed-cpu/NTI-Node.js

--- customer_data.js
+++ customer_data.js
@@ -4,4 +4,45 @@ let cus_data=[{Name:"Mohamed Saeed",Email:"mohamed@gmail.com",Phone:"01067480967
 ;
 
 
-export default cus_data;    
+export default cus_data; 
+ 
+
+
+//format  to customer and treatment
+
+function formatBooking(cus_data){
+    return `${cus_data.Name} -${cus_data.Treatment} - ${cus_data.status}`;
+}
+
+function FormatBooking(cus_data){
+    return `${cus_data.Name} + ${cus_data.Phone}+${cus_data.Email}`
+ }
+
+
+
+
+//filter for customer complet
+const statusCompleted=cus_data.filter((customer)=>{
+    
+        return customer.status==="Completed"
+    
+    
+    
+ })
+
+ //filter wating status
+
+const statusWaiting=cus_data.filter((customer)=>{
+    return customer.status==="Waiting";
+
+})
+console.log(statusCompleted)
+
+
+///checed for name 
+
+cus_data.map(formatBooking)
+
+cus_data.map(FormatBooking)
+export {formatBooking, statusCompleted,statusWaiting    }
+
